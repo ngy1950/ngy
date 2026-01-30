e8aa96b0c75533a52ba86bbfd09a80ef27698e60
skaru <ngy1950@kakao.com>
2026-01-30 23:07:55 +0900

    견적의뢰 기능 commit

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -24,3 +24,52 @@ $util.alert = (title, msg, iconType) => {
     });
 
 }
+
+$(document).ready(() => {
+    $('#reservationButton').click(() => {
+        reservation();
+    });
+
+    $('a[data-bs-toggle="modal"]').bind('click', () => {
+        $('#modalDiv').css('display','block')
+    });
+
+    $('button[data-bs-dismiss="modal"]').bind('click', () => {
+         $('#modalDiv').css('display','none')
+    });
+});
+
+$(document).keydown(function(event) {
+    if ( event.keyCode == 27 || event.which == 27 ) {
+        $('#modalDiv').css('display','none')
+    }
+});
+
+var phoneCheck = (phoneNumber) => {
+  phoneNumber.value = phoneNumber.value.replace(/[^0-9]/g, '').replace(/(^02.{0}|^01.{1}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/g, "$1-$2-$3");
+}
+
+
+var reservation = () => {
+
+  let formData = $util.serializeObject($('#contactForm'));
+
+  $.ajax({
+      type : 'post',
+      url : '/api/v1/reservation.do',
+      async : true,
+      headers : {
+          "Content-Type" : "application/json",
+          "X-HTTP-Method-Override" : "POST"
+      },
+      dataType : 'text',
+      data : JSON.stringify(formData),
+      success : function(result){
+        debugger;
+        result == 'Y' ? $util.alert('', '최대한 빠르게 연락드리겠습니다. 감사합니다.', 'success') : $util.alert('다시 시도해주세요.', '', 'warning');
+      },
+      error : function (request, status, error) {
+        $util.alert('다시 시도해주세요.', '', 'warning');
+      }
+  });
+}
