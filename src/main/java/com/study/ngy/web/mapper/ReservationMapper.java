5c8c8545bfa3d4ce0ed893d20de66b1153fafb6b
skaru <ngy1950@kakao.com>
2026-03-05 21:32:02 +0900

    미사용 DB/카카오 푸시 관련 파일 삭제

--- src/main/java/com/study/ngy/web/mapper/ReservationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.study.ngy.web.mapper;
-
-import com.study.ngy.web.model.ReservationDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ReservationMapper {
-    int insertReservation (ReservationDTO param);
-}
