e719eda2aecca11fdd126711fb3524cf9fbda235
skaru <ngy1950@kakao.com>
2026-03-11 22:13:00 +0900

    방문자 접속시간 한국시간으로 표기

--- src/main/java/com/study/ngy/NgyApplication.java
+++ src/main/java/com/study/ngy/NgyApplication.java
@@ -3,10 +3,13 @@ package com.study.ngy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class NgyApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(NgyApplication.class, args);
 	}
 
